46dcfa8c47cf83560a319c689d44df44677947c2
pablo.cruz <pablo.cruz@ppm.com.ec>
2022-12-26 18:13:20 -0500

    Correct spaces in print service

--- src/main/java/org/example/infrastructure/printBoardService/PrintScoreBoardService.java
+++ src/main/java/org/example/infrastructure/printBoardService/PrintScoreBoardService.java
@@ -11,7 +11,7 @@ public class PrintScoreBoardService implements IPrintScoreBoardService {
     public Void printScoreBoardRow(List<ScoreBoardRow> scoreBoard) {
         System.out.print("Frame         ");
         for(int i = 1; i <= 10; i++){
-            System.out.print(i+ "       ");
+            System.out.print(i+ "      ");
         }
         System.out.println();
         for (ScoreBoardRow row : scoreBoard){
@@ -25,30 +25,29 @@ public class PrintScoreBoardService implements IPrintScoreBoardService {
                     complementValue = row.getRegularSlots()[i].getSecondShot();
                     complement = 10 - value;
                     if(complement == complementValue){
-                        System.out.print(value + "  /   ");
+                        System.out.print("  "+value + "  /  ");
                     }else{
-                        System.out.print(value + "    " + complementValue + "   ");
+                        System.out.print("  "+value + "  " + complementValue + "  ");
                     }
                 }else if (value ==10 && i <10){
-                    System.out.print("  X   ");
+                    System.out.print("  X  ");
                 }
                 if(i == 10){
                     int value1 = row.getRegularSlots()[i].getFirstShot();
                     int value2 = row.getRegularSlots()[i].getSecondShot();
                     int value3 = row.getRegularSlots()[i].getThirdShot();
-                    System.out.print(value1==10? "X     ": value1+ "    ");
-                    System.out.print(value2==10? "X     ": value2+ "    ");
-                    System.out.println(value3==10? "X   ": value3+ "    ");
+                    System.out.print(value1==10? "  X  ": value1+ "  ");
+                    System.out.print(value2==10? "  X  ": value2+ "  ");
+                    System.out.println(value3==10? "  X  ": value3+ "  ");
                 }
             }
-            System.out.print("Score    ");
+            System.out.print("Score       ");
             for (int i = 1;i <= 10; i ++){
                 int result = row.getRegularSlots()[i].getResult();
-                System.out.print(result + "     ");
-                if(i == 10){
-                    System.out.println(result + "     ");
-                }
+                System.out.print("  "+ result + "   ");
             }
+            System.out.println();
+
         }
         return null;
     }
